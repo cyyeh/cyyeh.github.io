df4f3624edd38aa71ba05947ca673dcbe0e04c21
ChihYu Yeh <cyyeh@data-sci.info>
2022-02-15 14:03:10 +0800

    fix broken links

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,7 +128,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `/img/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/img/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
